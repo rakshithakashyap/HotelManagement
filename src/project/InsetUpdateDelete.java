8227b1a3804824457bd742b24788c07260defd09
rakshithakashyap <54228221+rakshithakashyap@users.noreply.github.com>
2021-07-27 19:56:00 +0530

    nth commit

---
+++ src/project/InsetUpdateDelete.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+import java.awt.HeadlessException;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Rakshitha.R
+ */
+public class InsetUpdateDelete {
+   public static void setData(String Query , String msg) {
+    Connection con = null;
+    Statement st = null;
+    try
+    {
+    con=ConnectionProvider.getCon();
+    st=con.createStatement();
+    st.executeUpdate(Query);
+    if(!msg.equals(""))
+        JOptionPane.showMessageDialog(null, msg);
+    }
+    catch(HeadlessException | SQLException e)
+    {
+        JOptionPane.showMessageDialog(null, e);
+    }
+    finally
+    {
+      try
+     {
+    
+    }
+    catch(Exception e)
+    {
+    
+    }
+    }
+}
+        }
